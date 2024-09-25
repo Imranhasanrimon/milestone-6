d5cabf018907caeeef001520ba53787cc51fe537
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-26 00:13:11 +0600

    spread ooperator

--- app.js
+++ app.js
@@ -14,22 +14,26 @@
 // const sum = add(5)
 
 //arrow function----------
-function add(a, b) {  //func declaration
-    return a + b;
-}
+// function add(a, b) {  //func declaration
+//     return a + b;
+// }
 
-const add2 = function (a, b) { //func expression
-    return a + b;
-}
-/* arrow function is like function expression */
-const add3 = (a, b) => a + b; //arrow function
+// const add2 = function (a, b) { //func expression
+//     return a + b;
+// }
+// /* arrow function is like function expression */
+// const add3 = (a, b) => a + b; //arrow function
 
-const getAge = (age) => age.age;
-const person = {
-    name: 'imrna hasan',
-    age: 23
-}
+// const getAge = (age) => age.age;
+// const person = {
+//     name: 'imrna hasan',
+//     age: 23
+// }
 
-//no parenthesis for single parameter
-const myfunc = perm => perm + 5;
-console.log(myfunc(5));
+// //no parenthesis for single parameter
+// const myfunc = perm => perm + 5;
+// console.log(myfunc(5));
+
+//...spread operator----------
+const numbers = [12, 25, 45, 35, 89, 75];
+console.log(Math.max(...numbers));
