c9afa541ece9c48e53036fde2343718288bf9d78
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-25 22:59:15 +0600

    consequence of no parameter

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // module 30
 //ES6 features___________________
-const money = 30;
-const rich = money + 50;
-console.log(money);
-console.log(rich);
+// const money = 30;
+// const rich = money + 50;
+// console.log(money);
+// console.log(rich);
+
+//default parameter
+function add(num1, num2) {
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result
+}
+const sum = add(5)
