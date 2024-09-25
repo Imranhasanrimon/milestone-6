7c6b90ad13cc6e932806388532a4b44616abf014
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-26 01:17:01 +0600

    Object.key,values,entries,freeze,seal

--- app.js
+++ app.js
@@ -40,17 +40,36 @@
 // console.log(maxNumber);
 
 // Destructuring----------
+// const person = {
+//     name: 'imrna hasan',
+//     age: 23,
+//     profession: 'full stack engineer',
+//     isMarried: true
+// }
+// const { name, age } = person; //destructuring by property name
+// const { profession, age: boyos } = person; //destructuring by diffrent name
+// //for array destructuring
+// const friends = ['saad', 'najmul', 'ariful', 'abdur rahman', 'taijul', 'ibrahim', 'zahid']
+// const [best, better] = friends;
+// const [, , good, normal] = friends; //skiping by commas
+// console.log(best);
+// console.log(better, good, normal);
+
+//keys----------
 const person = {
     name: 'imrna hasan',
     age: 23,
     profession: 'full stack engineer',
     isMarried: true
 }
-const { name, age } = person; //destructuring by property name
-const { profession, age: boyos } = person; //destructuring by diffrent name
-//for array destructuring
-const friends = ['saad', 'najmul', 'ariful', 'abdur rahman', 'taijul', 'ibrahim', 'zahid']
-const [best, better] = friends;
-const [, , good, normal] = friends; //skiping by commas
-console.log(best);
-console.log(better, good, normal);
+// const keys = Object.keys(person);
+// const values = Object.values(person)
+// // console.log(...keys, ...values); //spread recape
+
+// //two dymensional array or array of array
+// const pair = Object.entries(person)
+// console.log(pair);
+console.log(person);
+// delete person.isMarried;  //by delete keyword
+const { isMarried, ...newPersonObj } = person; //by destructuring form main
+console.log(newPersonObj);
