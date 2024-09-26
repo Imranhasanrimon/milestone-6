d240604a3a7c219cdeb3580ce6274bc3902c4c49
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-27 00:02:34 +0600

    array map

--- module-31.js
+++ module-31.js
@@ -1,1 +1,8 @@
-console.log('test');
+// Array Map-------
+const nums = [2, 5, 3, 7];
+function dob(num) {
+    // console.log(num);
+    return num * 2;
+}
+console.log(nums.map(dob));
+console.log(nums.map(n => n * 3));
