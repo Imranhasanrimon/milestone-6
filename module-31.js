ca07a86686602b5829a19b0ceef32185695ed5b5
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-27 11:23:01 +0600

    task one is done

--- module-31.js
+++ module-31.js
@@ -47,3 +47,28 @@
 
 // const { x, y, z } = { x: 1, y1: 2, z: 3 };
 // console.log(y);
+
+/*         practice from Github          */
+// task -1: console log the secondary school location of Sophia 
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [
+                        { school_name: "ABC primary school" },
+                        { location: "Peters burg" }
+                    ]
+            },
+            {
+                secondary:
+                    [
+                        { school_name: "ABC secondary school" },
+                        { location: "St Lorence" }
+                    ]
+            },
+        ]
+    }
+}
+console.log(data.Sophia.study[1].secondary[1].location);
