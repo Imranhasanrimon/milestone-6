4ad30f257401644257e9766947f06a97ff46674c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-27 11:13:30 +0600

    module 31 tutorial and quiz is done

--- module-31.js
+++ module-31.js
@@ -20,14 +20,30 @@
 
 
 //JS Class
-class Friend {
-    constructor(name, aim, catagory) {
-        this.Name = name;
-        this.Vision = aim;
-        this.Type = catagory;
-    }
-}
-const najmul = new Friend('Najmul', 'Higher Study', 'Best');
-console.log(najmul);
-const saad = new Friend('Saad', 'IT Guy', 'Best');
-console.log(saad);
+// class Friend {
+//     constructor(name, aim, catagory) {
+//         this.Name = name;
+//         this.Vision = aim;
+//         this.Type = catagory;
+//     }
+//     activity(name) {
+//         return name + ' is working hard'
+//     }
+// }
+// const najmul = new Friend('Najmul', 'Higher Study', 'Best');
+// console.log(najmul);
+// const saad = new Friend('Saad', 'IT Guy', 'Best');
+// console.log(saad);
+
+// const nums = [1, 2, 3, 4, 5];
+// let output = nums.filter(n => n % 2);
+// console.log(output);
+
+// function min(nums) { return Math.min(nums) }
+// console.log(min(...[1, 3, 2]));
+
+// const cube = x => x * x * x;
+// console.log(cube(2))
+
+// const { x, y, z } = { x: 1, y1: 2, z: 3 };
+// console.log(y);
