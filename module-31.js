caf2a50d46e33a7f575d7901302e539c11a9166a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-27 09:17:22 +0600

    map and filter is done

--- module-31.js
+++ module-31.js
@@ -1,8 +1,12 @@
 // Array Map-------
-const nums = [2, 5, 3, 7];
+const nums = [2, 5, 3, 7, 50, 39, 13];
 function dob(num) {
     // console.log(num);
     return num * 2;
 }
-console.log(nums.map(dob));
-console.log(nums.map(n => n * 3));
+// console.log(nums.map(dob));
+// console.log(nums.map(n => n * 3));
+
+//Array Filter------
+console.log(nums.filter(n => n > 9));
+console.log(nums.filter(n => n % 2 === 0));
