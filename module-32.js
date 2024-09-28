a56edd20decb88f10117a91cbefa330f2da9a760
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-28 08:45:07 +0600

    type coercion

--- module-32.js
+++ module-32.js
@@ -1,9 +1,14 @@
-let x = ['imran', 'hasan', 'rimon']
-let y = x;
-y[1] = 'ali'
-console.log(x, y);
+// let x = ['imran', 'hasan', 'rimon']
+// let y = x;
+// y[1] = 'ali'
+// console.log(x, y);
 
-function myFunc(a, b) {
-    const total = a + b;
-}
-console.log(myFunc(3, 5));
+// function myFunc(a, b) {
+//     const total = a + b;
+// }
+// console.log(myFunc(3, 5));
+
+//type coersion
+const a = 1;
+const b = true;
+console.log(b === a);
