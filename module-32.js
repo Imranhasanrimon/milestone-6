ee9ab861fbe175cc62d7b568ae3aabdfdb97421f
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-28 12:03:26 +0600

    module 32 is done

--- module-32.js
+++ module-32.js
@@ -109,7 +109,27 @@
 // change(team1, team2)
 // console.log(team1, team2);
 
-let p = 'Javascript';
-let q = p;
-p = 'React';
-console.log(q);
+// let p = 'Javascript';
+// let q = p;
+// p = 'React';
+// console.log(q);
+
+// const isTrue = true;
+// console.log(!isTrue ? "hello" : "world")
+
+// const sum = (p, q) => {
+//     p + q;
+// }
+// const result = sum(2, 3);
+// console.log(result);
+
+// if ("2" === 2) {
+//     console.log("Inside if");
+// } else {
+//     console.log("Inside else");
+// }
+
+// function work(x, y = 4) {
+//     return x + y;
+// }
+// console.log(work(32));
