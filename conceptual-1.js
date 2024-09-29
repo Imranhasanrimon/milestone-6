b48f8c74192a0f44e710133fd60e21feba645025
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-29 14:35:18 +0600

    logical operator

--- conceptual-1.js
+++ conceptual-1.js
@@ -5,8 +5,14 @@
 // }
 // console.log(x);
 
-if (true) {
-    let x = 5;
-    console.log(x);
-}
+// if (true) {
+//     let x = 5;
+//     console.log(x);
+// }
 // console.log(x);
+
+// console.log(6 + undefined);
+
+let x = true;
+console.log(x && false);               //logical operator
+console.log(x || 'or right side');
