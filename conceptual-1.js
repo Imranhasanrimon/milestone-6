9877b8dcaac0c3194128fd88987877f8a66b1d0d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-29 14:08:19 +0600

    block scope is done

--- conceptual-1.js
+++ conceptual-1.js
@@ -1,1 +1,12 @@
-console.log('testing');
+// console.log('testing');
+// if (true) {
+//     var x = 5;
+//     console.log(x);
+// }
+// console.log(x);
+
+if (true) {
+    let x = 5;
+    console.log(x);
+}
+// console.log(x);
