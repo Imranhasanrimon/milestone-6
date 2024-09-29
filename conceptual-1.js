e0d99c13d9cc066c09b897476ab4a7f72d69da9d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-29 14:42:59 +0600

    spread operator for copying purposes

--- conceptual-1.js
+++ conceptual-1.js
@@ -13,6 +13,14 @@
 
 // console.log(6 + undefined);
 
-let x = true;
-console.log(x && false);               //logical operator
-console.log(x || 'or right side');
+// let x = true;
+// console.log(x && false);               //logical operator
+// console.log(x || 'or right side');
+
+//spread operator for copying
+const arr1 = [1, 2, 3, 4];
+const arr2 = [...arr1];
+arr1.push(100);
+arr2.push(200)
+console.log(arr1);
+console.log(arr2);
