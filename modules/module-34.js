3d22e94932f6262d589a3bc6cba8116b401a7f3a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-02 12:22:52 +0600

    solving callback func problem 1

--- modules/module-34.js
+++ modules/module-34.js
@@ -1,23 +1,32 @@
-let message = 'Hello!';
-setTimeout(() => {
-    message = 'Goodbye!';
-}, 0);
-console.log(message);
+// let message = 'Hello!';
+// setTimeout(() => {
+//     message = 'Goodbye!';
+// }, 0);
+// console.log(message);
 
-// What is the output of the following JavaScript code using async/await?
+// // What is the output of the following JavaScript code using async/await?
 
-async function myFunction() {
-    return "Hello";
-}
-myFunction().then(console.log);
+// async function myFunction() {
+//     return "Hello";
+// }
+// myFunction().then(console.log);
 
-let x = 1;
-let intervalId = setInterval(() => {
-    console.log(x++);
-    if (x > 3) clearInterval(intervalId);
-}, 1000);
+// let x = 1;
+// let intervalId = setInterval(() => {
+//     console.log(x++);
+//     if (x > 3) clearInterval(intervalId);
+// }, 1000);
 
-console.log("A");
-Promise.resolve().then(() => console.log("B"));
-setTimeout(() => console.log("C"), 0);
-console.log("D");
+// console.log("A");
+// Promise.resolve().then(() => console.log("B"));
+// setTimeout(() => console.log("C"), 0);
+// console.log("D");
+
+
+//callback problems---------
+const greet = (name, callback) => {
+    console.log(callback(), name);
+};
+
+const callback = () => 'hellow';
+greet('Imran', callback)
