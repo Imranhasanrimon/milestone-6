2e21f0903db3a5a8fede7b55d51ae6d87df91cc3
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-03 14:55:02 +0600

    video card design

--- projects/ph-tube/scripts/video.js
+++ projects/ph-tube/scripts/video.js
@@ -13,6 +13,24 @@ const loadVideos = () => {
         .then(data => displayVideos(data.videos))
         .catch(err => console.log(err))
 }
+const demo = {
+    "category_id": "1003",
+    "video_id": "aaaf",
+    "thumbnail": "https://i.ibb.co/5LRQkKF/stick-and-stones.jpg",
+    "title": "Sticks & Stones",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/rdTZrCM/dev.jpg",
+            "profile_name": "Dave Chappelle",
+            "verified": true
+        }
+    ],
+    "others": {
+        "views": "113K",
+        "posted_date": ""
+    },
+    "description": "Dave Chappelle's 'Sticks & Stones' has garnered 113K views and remains a controversial yet highly engaging piece of stand-up comedy. Known for his fearless approach, Dave dives into a wide range of topics, delivering his unique perspective with wit and sharp humor. As a verified artist, Dave's comedy is raw, honest, and unapologetically funny."
+}
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos')
@@ -20,17 +38,26 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = 'card card-compact  shadow-sm border'
         card.innerHTML = `
-       <figure>
+       <figure class="h-[200px] relative">
     <img
-      src="${item.thumbnail}"
+      src="${item.thumbnail}" class="h-full w-full object-cover"
       alt="Shoes" />
+      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${item.others.posted_date}</span>
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="px-0 py-2 flex gap-2">
+   <div>
+   <img class="w-10 h-10 rounded-full object-cover" src="${item.authors[0].profile_picture}">
+   </div>
+   <div>
+   <h2 class="font-bold">${item.title}</h2>
+   <div class="flex items-center gap-2">
+   <p class="text-gray-400">${item.authors[0].profile_name}</p>
+
+   ${item.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=p9jKUHLk5ejE&format=png">` : ""}
+
+   </div>
+   <p></p>
+   </div>
   </div>
        `
         videoContainer.append(card)
