48911983dd6e3047832e0644ee7321f2d9982e4d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-04 18:37:15 +0600

    handling search value

--- projects/phone-hunter-API/app.js
+++ projects/phone-hunter-API/app.js
@@ -1,16 +1,36 @@
-const loadAllPhones = async () => {
+const loadAllPhones = async (status, brandName = "") => {
     document.getElementById('spinner').classList.add('hidden');
-
+    console.log(brandName);
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
     const data = await response.json();
-    console.log(data);
+
+    if (status) {
+        console.log(data.data);
+    } else {
+        displayAllPhones(data.data);
+    }
+}
+
+const displayAllPhones = (phones) => {
+    // console.log(phones.slice(0, 6));
 
 }
+
+
+const handleShowAll = () => {
+    loadAllPhones(true)
+}
+
+
+
 
 const handleSearch = () => {
-
+    const inputValue = document.getElementById('input-value').value
+    console.log(inputValue);
     document.getElementById('spinner').classList.remove('hidden')
     setTimeout(() => {
-        loadAllPhones()
+        loadAllPhones(false, inputValue)
     }, 700)
 }
+
+loadAllPhones()
