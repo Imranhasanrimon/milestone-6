7aa67603971ca2d00448bc69e81a18fe15fedd00
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-04 10:38:26 +0600

    adding load details function

--- projects/ph-tube/scripts/video.js
+++ projects/ph-tube/scripts/video.js
@@ -15,6 +15,14 @@ const removeActiveCalass = () => {
         btn.classList.remove('bg-red-500', 'text-white')
     }
 }
+
+const loadDetails = async (videoId) => {
+    const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    const res = await fetch(uri);
+    const data = await res.json()
+    console.log(data);
+
+}
 const loadCategories = () => {
     //fetch the data
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -82,12 +90,11 @@ const displayVideos = (videos) => {
    ${item.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=p9jKUHLk5ejE&format=png">` : ""}
 
    </div>
-   <p><button class="btn btn-sm btn-error">Details</button></p>
+   <p><button onclick="loadDetails('${item.video_id}')" class="btn btn-sm btn-error">Details</button></p>
    </div>
   </div>
        `
         videoContainer.append(card)
-        // console.log(item);
     })
 }
 
