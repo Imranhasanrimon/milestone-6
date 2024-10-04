6447f582e7481edb79631617947c0fe4ae9660b9
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-04 19:48:01 +0600

    creating dynamic cards

--- projects/phone-hunter-API/app.js
+++ projects/phone-hunter-API/app.js
@@ -1,24 +1,46 @@
-const loadAllPhones = async (status, brandName = "") => {
+const loadAllPhones = async (status, brandName) => {
     document.getElementById('spinner').classList.add('hidden');
-    console.log(brandName);
-    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName ? brandName : 'iphone'}`);
     const data = await response.json();
 
     if (status) {
         console.log(data.data);
     } else {
-        displayAllPhones(data.data);
+        displayAllPhones(data.data.slice(0, 6));
     }
 }
 
 const displayAllPhones = (phones) => {
-    // console.log(phones.slice(0, 6));
+    const phoneContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
 
+        const { brand, image, slug } = phone;
+        console.log(brand, image, slug);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card card-compact bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="${image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${brand}</h2>
+    <p>${slug}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Show Details</button>
+    </div>
+  </div>
+</div>
+        `
+        phoneContainer.append(div)
+    });
 }
 
 
 const handleShowAll = () => {
     loadAllPhones(true)
+
 }
 
 
@@ -33,4 +55,4 @@ const handleSearch = () => {
     }, 700)
 }
 
-loadAllPhones()
+loadAllPhones(false, 'iphone')
