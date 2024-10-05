c66791ba823f2d7c7b14c598964d327ba395129a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-05 09:56:33 +0600

    loading all post through API

--- projects/B9A6/app.js
+++ projects/B9A6/app.js
@@ -1,10 +1,14 @@
-fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
-    .then(res => res.json())
-    .then(data => console.log(data))
 
 
 
+const loadAllPosts = async () => {
 
+    const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
+    const data = await response.json();
+    console.log(data);
+}
+
+loadAllPosts()
 
 
 
