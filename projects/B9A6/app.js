63093d92484825c3e7859a3ae04de62538b6fc5d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-05 09:53:32 +0600

    creating markup for each post

---
+++ projects/B9A6/app.js
@@ -0,0 +1,32 @@
+fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //latest post
+// fetch(" https://openapi.programming-hero.com/api/retro-forum/latest-posts")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
