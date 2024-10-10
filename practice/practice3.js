056358a5fa6348af9fc0ed113eced69545dbd325
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-10 23:44:16 +0600

    countdown modal practice is done

--- practice/practice3.js
+++ practice/practice3.js
@@ -3,10 +3,30 @@
 //     .then(res => res.json())
 //     .then(data => console.log(data))
 
-my_modal_5.showModal()
+// setTimeout(() => {
+//     // document.getElementById('my_modal_5').close()
+//     // my_modal_5.close()
+//     console.log('time over');
+// }, 1500)
 
+// let counter = 3;
+// const helloText = document.getElementById('hello');
+// const setIntervalId = () => {
+//     setInterval(() => {
+//         counter--;
+//         helloText.innerText = counter;
+//         console.log('1s has gone');
+//     }, 1000)
+//     clearInterval(setIntervalId)
+// }
 
-setTimeout(() => {
-    // my_modal_5.close()
-    console.log('time over');
-}, 1000)
+// const asyncFunc = () => {
+//     setIntervalId()
+//     setTimeout(() => {
+//         console.log('time over');
+//         clearInterval(setIntervalId);
+//         setTimeout(() => {
+//             my_modal_5.close()
+//         }, 1000);
+//     }, 2000);
+// }
