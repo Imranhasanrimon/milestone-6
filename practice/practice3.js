eec9669469630d53cab604506dd8ea433d46da27
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-10 22:55:19 +0600

    closing modal by object method

--- practice/practice3.js
+++ practice/practice3.js
@@ -2,3 +2,11 @@
 // fetch('https://openapi.programming-hero.com/api/peddy/pets')
 //     .then(res => res.json())
 //     .then(data => console.log(data))
+
+my_modal_5.showModal()
+
+
+setTimeout(() => {
+    // my_modal_5.close()
+    console.log('time over');
+}, 1000)
