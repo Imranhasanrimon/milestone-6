8ebb0d44d0f8a7b628f4363e981cfe273f22578d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-12 15:45:10 +0600

    remove item from local storage

--- modules/module-36.js
+++ modules/module-36.js
@@ -1,12 +1,13 @@
 // const setAge = localStorage.setItem('Age', 23);
 // console.log(localStorage.getItem('Age'));
 
-const obj = {
-    name: "Imran Hasan",
-    Age: 23,
-    Profession: "Web Developer",
-}
+// const obj = {
+//     name: "Imran Hasan",
+//     Age: 23,
+//     Profession: "Web Developer",
+// }
 
-localStorage.setItem('person', JSON.stringify(obj))
+// localStorage.setItem('person', JSON.stringify(obj))
 
-console.log(JSON.parse(localStorage.getItem('person')));
+// console.log(JSON.parse(localStorage.getItem('person')));
+localStorage.removeItem('name')
