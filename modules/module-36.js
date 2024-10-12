f835446d84d1b117c61d4d1ae9788ea865b789fc
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-12 10:34:07 +0600

    set and get object from local storage

---
+++ modules/module-36.js
@@ -0,0 +1,12 @@
+// const setAge = localStorage.setItem('Age', 23);
+// console.log(localStorage.getItem('Age'));
+
+const obj = {
+    name: "Imran Hasan",
+    Age: 23,
+    Profession: "Web Developer",
+}
+
+localStorage.setItem('person', JSON.stringify(obj))
+
+console.log(JSON.parse(localStorage.getItem('person')));
