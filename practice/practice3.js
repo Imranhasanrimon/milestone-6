daa9f942c83d8d69cacdb800d18e6b82afe9efdd
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-13 19:58:55 +0600

    addng some code

--- practice/practice3.js
+++ practice/practice3.js
@@ -168,5 +168,34 @@
 // // Call outer function here
 // outer()
 
-console.log(x);  // What will this log?
-var x = 5;
+// console.log(x);  // What will this log?
+// var x = 5;
+
+// function end() {
+//     console.log("End");
+// }
+// start()
+// // Call start function here
+
+// function outer() {
+//     let x = "Outer";
+//     // Call inner function here
+//     inner()
+// }
+
+// function inner() {
+//     let x = "Inner";
+//     // Call display function here
+//     display()
+// }
+
+// function display() {
+//     // Print x here
+//     console.log(x);
+// }
+
+// // Call outer function here
+// outer()
+
+// console.log(x);  // What will this log?
+// var x = 5;
